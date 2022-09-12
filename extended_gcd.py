a9f462f13240e7683bfbd6e0f43ed16214af3213
cruzryan <32502734+cruzryan@users.noreply.github.com>
2022-09-11 23:37:30 -0500

    extended gcd

---
+++ extended_gcd.py
@@ -0,0 +1,12 @@
+def extended_gcd(a, b):
+    if a == 0 :
+        return b,0,1
+             
+    gcd,x1,y1 = extended_gcd(b%a, a)
+    x = y1 - (b//a) * x1
+    y = x1
+     
+    return gcd,x,y
+
+g, x, y = extended_gcd(26513, 32321)
+print("u: ", x, "v:", y)

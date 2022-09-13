89f658d955c2bd6d7e3b11c4518558623031478e
cruzryan <32502734+cruzryan@users.noreply.github.com>
2022-09-13 00:59:49 -0500

    finding multiplicative inverse!

---
+++ mod2.py
@@ -0,0 +1,11 @@
+#Multiplicative inverse of g
+#3 * d = 1 mod 13
+
+def mult_inv(i1, mod):
+    d = 0
+    for i in range(2,100):
+        if i*i1 % mod == 1:
+            return i
+    return d
+
+print(mult_inv(3,13))
